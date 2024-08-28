26214897cf83670acae1e077c426e3e063dd6451
vraj <vrajhp0254@gmail.com>
2024-08-28 21:53:10 +0530

    add map

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-const express = require('express');
-const socketio = require('socket.io')
-const http= require('http');
-const path = require('path');
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.set('view engine', 'ejs');
-app.set(express.static(path.join(__dirname,'public')))
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-io.on('connection', (socket) => {
-    console.log('New WS Connection');
+io.on("connection", (socket) => {
+    socket.on("send-Location", (data) => {
+        io.emit(
+            "recieve-Location",
+            {id: socket.id, ...data}
+        );
+    });
+    console.log("New WS Connection");
 });
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 server.listen(3000, () => {
-    console.log('Server running on port 3000');
-})
+  console.log("Server running on port 3000");
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,27 @@
 const socket = io();
+
+if(navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        console.log(latitude, longitude);
+        socket.emit('send-Location', { latitude, longitude });
+    },
+(error)=>{console.log(error)},{
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+}
+);
+}
+
+
+const map = L.map("map").setView([0, 0], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+const market ={}
+
+socket.on("recieve-Location", (coords) => {
+    const { id, latitude, longitude } = coords;
+    map.setView([latitude, longitude], 10);
+})
